87938197b18d626fdcc0666458bf599d4c220b25
Yashbhai90 <167395535+Yashbhai90@users.noreply.github.com>
2024-05-02 14:49:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,6 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/Learningbots79/LB_Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
